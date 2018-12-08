bec8ba9165a90a6dd799a75f5b921e6a698bc318
Ant Milner <executivesales@hotmail.co.uk>
2018-12-08 13:10:58 +0000

    view ill appear

--- udder/ViewController.swift
+++ udder/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func canPerformUnwindSegueAction(_ action: Selector, from fromViewController: UIViewController, withSender sender: Any) -> Bool {
+        <#code#>
+    }
 
 
 }
